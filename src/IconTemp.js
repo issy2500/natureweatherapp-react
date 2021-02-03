37ce6723c7ec394dabf9c5c0c07005891a51a5a9
issy2500 <66333080+issy2500@users.noreply.github.com>
2021-02-03 15:34:05 -0500

    mapped function in IconTemp.js

--- src/IconTemp.js
+++ src/IconTemp.js
@@ -1,13 +1,68 @@
 import React from "react";
 import "./IconTemp.css";
+import {Cloudy,Snow, Sunny, Rain} from "weather-styled-icon";
+import {WeatherThemeProvider} from "weather-styled-icon";
 
-
+export const YELLOW = '#FFEB3B';
+export const AMBER = '#FFC107';
+export const GREY = '#607D8B';
+export const WHITE = '#FFFFFF';
+export const LIGHT_BLUE = '#2196F3';
+export const INDIGO_LIGHT = '#E8EAF6';
+export const RED ='#FF4F5E';
 
 
 export default function IconTemp(props){
+    
+  const codeMapping={
+    "01d": <Sunny />,
+    "01n": <Sunny />, // change the color of the sun to a dark one
+    "02d": <Cloudy />,
+    "02n": <Cloudy />,
+    "03d": <Cloudy />,
+    "03n": <Cloudy />,
+    "04d": <Cloudy />,
+    "04n": <Cloudy />,
+    "09d": <Rain />,
+    "09n": <Rain />,
+    "10d": <Rain />,
+    "10n": <Rain />,
+    "11d": <Snow />,
+    "11n": <Snow />,
+    "13d": <Snow />,
+    "13n": <Snow />,
+    "50d": <Cloudy />,
+    "50n": <Cloudy />,
+   
+  }
+
+  const myCustomTheme = {
+  sunColor: RED,
+  raysColor: RED,
+  cloudsColor: GREY,
+  dropsColor: RED,
+  snowColor: LIGHT_BLUE,
+  boltColor: LIGHT_BLUE,
+  backgroundColor: WHITE,
+  
+};
+
+    const App = () => (
+  <div>
+    <WeatherThemeProvider theme={myCustomTheme}>
+    <Sunny/>
+    <Cloudy />
+    <Rain />
+    <Snow />
+    </WeatherThemeProvider>
+  </div>
+);
+
+
+
 return(
 <div className="IconTemp">
-<img src ={props.icon}/>
+{codeMapping[props.icon]}
 <span className="celsius-temp">
 {props.temperature}    
     </span> °C
