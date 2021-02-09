2f4dba54edaec08ce1f6aa889d14a94008f36b8b
issy2500 <66333080+issy2500@users.noreply.github.com>
2021-02-09 11:03:52 -0500

    Fixed App.js links

--- src/App.js
+++ src/App.js
@@ -11,12 +11,12 @@ export default function App() {
      <Weather/>
       <footer>
         This project is was created by {""}
-        <a href="https://linkedin.com/in/isabel-castillo " target ="_blank" rel="nonreferrer">
+        <a href="https://linkedin.com/in/isabel-castillo " target ="_blank" rel="noreferrer">
           Isabel Castillo  {""}
           </a> 
           and is open sourced by GitHub {""}
       <a href = "https://github.com/issy2500/natureweatherapp-react" 
-      target = "_blank" rel="nonreferrer">
+      target = "_blank" rel="noreferrer">
         Repository
 
       </a> 
